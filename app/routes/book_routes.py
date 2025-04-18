22d1bfd5a783b42d37b8b95d8c2d4dcf0de6de68
eugeniakashanskaya <jkashanskaya@gmail.com>
2025-04-17 20:40:26 -0700

    Simplifies get_one_book

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -18,22 +18,32 @@ def get_all_books():
         )
     return books_response
 
+# @books_bp.get("/<book_id>")
+# def get_one_book(book_id):
+#     try: # add try/except to handle cases when Error occurs
+#         book_id = int(book_id)
+#     except: 
+#         return {"message": f"book {book_id} invalid"}, 400
+
+#     for book in books:
+#         if book.id == book_id:
+#             return {
+#                 "id": book.id,
+#                 "title": book.title,
+#                 "description": book.description
+#             }
+        
+#     return {"message": f"book {book_id} not found"}, 404
+
 @books_bp.get("/<book_id>")
 def get_one_book(book_id):
-    try: # add try/except to handle cases when Error occurs
-        book_id = int(book_id)
-    except: 
-        return {"message": f"book {book_id} invalid"}, 400
+    book = validate_book(book_id)
 
-    for book in books:
-        if book.id == book_id:
-            return {
+    return {
                 "id": book.id,
                 "title": book.title,
-                "description": book.description
-            }
-        
-    return {"message": f"book {book_id} not found"}, 404
+                "description": book.description    
+    }
 
 def validate_book(book_id): # add helper function
     try: 
