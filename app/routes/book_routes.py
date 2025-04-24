53adbba943e8b00f9651bc3f99bd459167592ddf
eugeniakashanskaya <jkashanskaya@gmail.com>
2025-04-23 18:30:26 -0700

    Adds create_book and refactors get_all_books

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,7 +1,44 @@
-from flask import Blueprint, abort, make_response
+from flask import Blueprint, abort, make_response, request
+from app.models.book import Book
+from ..db import db
 # from app.models.book import books
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+
+@books_bp.post("")
+def create_book():
+    request_body = request.get_json()
+    title = request_body["title"]
+    description = request_body["description"]
+
+    new_book = Book(title=title, description=description)
+    db.session.add(new_book)
+    db.session.commit()
+
+    response = {
+        "id": new_book.id,
+        "title": new_book.title,
+        "description": new_book.description,
+    }
+    return response, 201
+
+@books_bp.get("")
+def get_all_books():
+    query = db.select(Book).order_by(Book.id)
+    books = db.session.scalars(query)
+    # We could also write the line above as:
+    # books = db.session.execute(query).scalars()
+
+    books_response = []
+    for book in books:
+        books_response.append(
+            {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
+        )
+    return books_response
 
 # # @books_bp.get("")
 # @books_bp.get("", strict_slashes=False)
