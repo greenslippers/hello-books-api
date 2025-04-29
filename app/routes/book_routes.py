afd624bfa2c1137a7970c41a68dc54d9610e03cf
eugeniakashanskaya <jkashanskaya@gmail.com>
2025-04-28 19:39:11 -0700

    added PUT & DELETE

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request, Response
 from app.models.book import Book
 from ..db import db
 # from app.models.book import books
@@ -42,28 +42,49 @@ def get_all_books():
 
 @books_bp.get("/<book_id>")
 def get_one_book(book_id):
-
     book = validate_book(book_id)
-    
+
     return {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description    
+        "id": book.id,
+        "title": book.title,
+        "description": book.description,
     }
 
-def validate_book(book_id): # add helper function
-    try: 
+def validate_book(book_id):
+    try:
         book_id = int(book_id)
-    except: 
+    except:
         response = {"message": f"book {book_id} invalid"}
-        abort(make_response(response, 400))
+        abort(make_response(response , 400))
 
     query = db.select(Book).where(Book.id == book_id)
     book = db.session.scalar(query)
-
+    
     if not book:
         response = {"message": f"book {book_id} not found"}
         abort(make_response(response, 404))
+
+    return book
+
+@books_bp.put("/<book_id>")
+def update_book(book_id):
+    book = validate_book(book_id)
+    request_body = request.get_json()
+
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+@books_bp.delete("/<book_id>")
+def delete_book(book_id):
+    book = validate_book(book_id)
+    db.session.delete(book)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
 
 # # @books_bp.get("")
 # @books_bp.get("", strict_slashes=False)
