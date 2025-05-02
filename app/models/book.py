8b6bfe38360d89d3a2b6783a37af284954736f4d
eugeniakashanskaya <jkashanskaya@gmail.com>
2025-05-02 08:17:05 -0700

    refatored

--- app/models/book.py
+++ app/models/book.py
@@ -6,6 +6,22 @@ class Book(db.Model):
     title: Mapped[str]
     description: Mapped[str]
 
+    # indented under the Book class definition
+    @classmethod
+    def from_dict(cls, book_data):
+        new_book = Book(title=book_data["title"],
+                        description=book_data["description"])
+        return new_book
+    
+    # indented under the Book class definition
+    def to_dict(self):
+        book_as_dict = {}
+        book_as_dict["id"] = self.id
+        book_as_dict["title"] = self.title
+        book_as_dict["description"] = self.description
+
+        return book_as_dict
+
 
 # class Book:
 #     def __init__(self, id, title, description):
